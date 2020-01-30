ceca715ee04df897553fcd5d4818ef41bda7bc77
Tretjak <alexander.andreasson1996@gmail.com>
2020-01-30 12:20:49 +0100

    Börjat programmer på uppgiften

--- sammanslagning.cpp
+++ sammanslagning.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 int main (){
-ofstream.G6-Konfliktlosning-CPP.md
-G6-Konfliktlossning-CPP.md <<"HELLO WOLRD \n";
-G6-konfliktlosning-CPP.md.close()
-
+string ptext;
+fstream presentation;
+presentation.open ("G1-Presentation-CPP.md", ios::in | ios::out | ios::app);
+while (presentation.is_open()){
+getline(presentation, ptext);
+cout << ptext << endl;
+if (ptext==""){
+presentation.close();
+}
+}
 }
