7b599fe1b7996ad330e844eb56250328d3d85074
Jerktool <h19joad@axevalla.fhsk.se>
2020-01-30 12:00:08 +0100

    La till kod för sammanslagning

--- sammanslagning.cpp
+++ sammanslagning.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
-int main (){
-ofstream.G6-Konfliktlosning-CPP.md
-G6-Konfliktlossning-CPP.md <<"HELLO WOLRD \n";
-G6-konfliktlosning-CPP.md.close()
+int main ()
+{
+
+    string G1;
+    string G2;
+    string G3;
+
+    ifstream readG1 ("G1-Presentation-CPP.md");
+    ifstream readG2 ("G3-Fordjupning-CPP.md");
+    ifstream readG3 ("G5-Konfliktlosning-CPP.md");
+    ofstream write ("Test.txt", ios ::out | ios::app);
+
+    while(getline ( readG1, G1))
+    {
+        write << G1 << endl;
+    }
+
+    while(getline (readG2, G2) )
+    {
+        write << G2 << endl;
+    }
+
+    while(getline (readG3, G3) )
+    {
+        write << G3 << endl;
+    }
+
+    cout <<"Text kopierad till nytt dokument" << endl;
+
+    readG1.close();
+    readG2.close();
+    readG3.close();
+
+    write.close();
+
 
 }
