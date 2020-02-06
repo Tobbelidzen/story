3bc0d50c38281ba780b26dab2e8d2371084a5393
Tretjak <alexander.andreasson1996@gmail.com>
2020-02-06 11:44:12 +0100

    Uppdaterade kodningen. Nu Läggs första dokumentet in i Sammanslagning

--- sammanslagning.cpp
+++ sammanslagning.cpp
@@ -4,14 +4,32 @@
 using namespace std;
 
 int main (){
-string ptext;
-fstream presentation;
-presentation.open ("G1-Presentation-CPP.md", ios::in | ios::out | ios::app);
-while (presentation.is_open()){
-getline(presentation, ptext);
-cout << ptext << endl;
-if (ptext==""){
-presentation.close();
-}
+    string ptext;
+    string stext="#Presentation";
+    string ntext;
+    fstream presentation;
+    fstream sammanslagning;
+
+    sammanslagning.open ("Sammanslagen-historia.md", ios::in | ios::out | ios::app);
+    presentation.open ("G1-Presentation-CPP.md", ios::in | ios::out | ios::app);
+
+    while (sammanslagning.is_open()){
+        getline(sammanslagning, ntext);
+
+        if (ntext==stext){
+
+            while(sammanslagning.is_open()){
+            getline(presentation,ptext);
+            sammanslagning << ptext << endl;
+
+                if (ptext==""){
+                    presentation.close();
+                    sammanslagning.close();
+
+                }
+        }
+
+
+    }
 }
 }
